6e4a4a5809c082a476e0e7dd583cb7eb9d4e53ca
shefat <shefatislam1946@gmail.com>
2023-07-06 13:25:52 +0600

    managed the images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['i.ytimg.com', 'yt3.ggpht.com', 'lh3.googleusercontent.com', 'yt3.googleusercontent.com'],
+    domains: ['i.ytimg.com', 'yt3.ggpht.com', 'lh3.googleusercontent.com', 'yt3.googleusercontent.com'],
+    unoptimized:true ,
     },
 }
 
